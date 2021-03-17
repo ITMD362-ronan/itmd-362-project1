943ca99f641e84ae3ebb665cd3a1724f6025ea17
ronan <rvalarese@gmail.com>
2021-03-17 00:01:03 -0400

    change form id and add validationEvent() function

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,3 +2,17 @@
     $('html').removeClass('nojs');
     $('html').addClass('hasjs');
   });
+
+  function validationEvent() {
+    var a = document.forms["pie"]["first_name"].value;
+    var b = document.forms["pie"]["last_name"].value;
+    var c = document.forms["pie"]["email"].value;
+    var d = document.forms["pie"]["phone_num"].value;
+    var e = document.forms["pie"]["birthday"].value;
+    if ((a == "") || (b == "") || (c == "") || (d == "") || (e == "")){
+      alert("Please fill in all sections");
+    }
+    else{
+      alert("Don't choke, time to EAT!");
+    }
+  }
